0e8d9fc7e404d4c6e113c6ba9d186db4b5375fff
Manasi Jadhav <ms.manasi.jadhav@gmail.com>
2025-06-07 12:44:57 +0530

    Celsius to Fahrenheit convertion

---
+++ 05.convert.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var c float32
+
+	fmt.Print("Enter the tempature is Celsius : ")
+	fmt.Scan(&c)
+
+	f := c*1.8 + 32
+
+	fmt.Println("The tempature in Fahrenheit is ", f)
+}
